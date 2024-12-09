df70aea4d0b2566e348f518187c4acda079d27e7
Morris Björni <pylysyly@gmail.com>
2024-12-09 23:11:09 +0200

    dynge e klart

--- Assets/Scripts/Projekt 5/CharacterSpawner.cs
+++ Assets/Scripts/Projekt 5/CharacterSpawner.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System.Collections;
 public class CharacterSpawner : MonoBehaviour
 {
     public GameObject Prefab;
@@ -7,11 +7,22 @@ public class CharacterSpawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = 5; i < Characters; i++)
-        {
-            Vector3 Position = new Vector3(Random.Range(-1, -2), -5, Random.Range(1, 2));
-            Instantiate(Prefab, Position, Prefab.transform.rotation);
-        }
+        StartCoroutine(spawn_dudes());
+
     }
+
+    IEnumerator spawn_dudes()
+    {
+        for(int i = 0; i < Characters; i++)
+        {
+            yield return new WaitForSeconds(2);
+            Vector3 Position = new Vector3(Random.Range(-1, -2), 0, Random.Range(1, 2));
+            Instantiate(Prefab, Position, Prefab.transform.rotation);
+            print(i);
+            
+        }
+       
+    }
+
 
 }
--- Assets/Scripts/Projekt 5/NPCController.cs
+++ Assets/Scripts/Projekt 5/NPCController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
@@ -5,16 +6,37 @@ using UnityEngine.AI;
 public class NPCController : MonoBehaviour
 {
     NavMeshAgent Agent;
-    GameObject WayPoint, WayPoint2;
+    GameObject WayPoint, WayPoint2, WayPoint3, WayPoint4, WayPoint5, WayPoint6, WayPoint7;
     Animator Anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    GameObject current_waypoint;
+
+    bool wait = false;
+    GameObject[] waypoints = new GameObject [7];
     void Start()
     {
         Agent = GetComponent<NavMeshAgent>();
         Anim = GetComponent<Animator>();
         WayPoint = GameObject.Find("Waypoint");
         WayPoint2 = GameObject.Find("Waypoint2");
+        WayPoint3 = GameObject.Find("Waypoint3");
+        WayPoint4 = GameObject.Find("Waypoint4");
+        WayPoint5 = GameObject.Find("Waypoint5");
+        WayPoint6 = GameObject.Find("Waypoint6");
+        WayPoint7 = GameObject.Find("Waypoint7");
+
+        
+
+        waypoints[0]=WayPoint;
+        waypoints[1]=WayPoint2;
+        waypoints[2]=WayPoint3;
+        waypoints[3]=WayPoint4;
+        waypoints[4]=WayPoint5;
+        waypoints[5]=WayPoint6;
+        waypoints[6]=WayPoint7;
+
         Agent.SetDestination(WayPoint.transform.position);
+        current_waypoint = WayPoint;
     }
 
     // Update is called once per frame
@@ -24,10 +46,14 @@ public class NPCController : MonoBehaviour
         {
             Anim.SetBool("Walking", true);
         }
-        else
+        
+        else if(wait == false)
         {
+            wait = true;
             Anim.SetBool("Walking", false);
-            StartCoroutine(DelayedFunctionCall(Random.Range(2, 6)));
+            StartCoroutine(DelayedFunctionCall(UnityEngine.Random.Range(1, 3)));
+            
+            
         }
     }
 
@@ -39,7 +65,13 @@ public class NPCController : MonoBehaviour
 
     void SetNextDestination()
     {
-        Agent.SetDestination(WayPoint2.transform.position);
-
+        if(Array.IndexOf(waypoints, current_waypoint)+1 == 6)
+        {
+            Destroy(this);
+            print ("kaka");
+        }
+        Agent.SetDestination(waypoints[Array.IndexOf(waypoints, current_waypoint)+1].transform.position);
+        current_waypoint = waypoints[Array.IndexOf(waypoints, current_waypoint) + 1];
+        wait = false;
     }
 }
